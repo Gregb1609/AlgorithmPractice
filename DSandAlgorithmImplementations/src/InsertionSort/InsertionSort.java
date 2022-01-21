65e6affd817808794f6550b99c6c69f5bbbc6a8e
Gregb1609 <Gregb2002uk@yahoo.co.uk>
2022-01-21 12:34:53 +0000

    Selection and insertion sort

---
+++ DSandAlgorithmImplementations/src/InsertionSort/InsertionSort.java
@@ -0,0 +1,29 @@
+package InsertionSort;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+	public static void main(String[] args) {
+		int [] values = {20,35,-15,7,55,1,-22};
+		System.out.println(Arrays.toString(values));
+		sort(values);
+		System.out.println(Arrays.toString(values));
+	}
+	
+	public static int[] sort(int[] input) {
+		for(int ordered = 1;ordered<input.length;ordered++) {
+			int temp=input[ordered];
+			for(int pointer = ordered;pointer>0;pointer--) {
+				if(input[pointer]>=input[pointer-1]) {
+					break;
+				}else {
+					input[pointer]=input[pointer-1];
+					input[pointer-1]=temp;
+					System.out.println(Arrays.toString(input));
+				}
+			}
+		}
+		return input;
+	}
+
+}
---
+++ DSandAlgorithmImplementations/src/SelectionSort/SelectionSort.java
@@ -0,0 +1,33 @@
+package SelectionSort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+	public static void main(String[] args) {
+		int [] values = {20,35,-15,7,55,1,-22};
+		System.out.println(Arrays.toString(values));
+		sort(values);
+		System.out.println(Arrays.toString(values));
+	}
+	
+	public static int[] sort(int[]input) {
+		for(int unsorted=input.length-1;unsorted>0;unsorted--) {
+			int highest = 0;
+			for(int pointer=0;pointer<unsorted;pointer++) {
+				
+				if(input[pointer]>input[highest]) {
+					highest=pointer;
+				}
+			}
+			swap(input,unsorted,highest);
+		}
+		return input;
+	}
+	public static void swap(int[] array,int location,int end) {
+		int temp = array[location];
+		array[location]=array[end];
+		array[end]=temp;
+	}
+
+}
